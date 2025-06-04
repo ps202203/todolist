77dff23746ffaa4eaea61dac67b236181b12afc2
ps202203 <pase375@gmail.com>
2025-06-04 23:46:42 +0900

    Set DB - MySQL, SpringTestCode

---
+++ src/main/java/com/mysite/todolist/DatabaseConnectionTest.java
@@ -0,0 +1,5 @@
+package com.mysite.todolist;
+
+public class DatabaseConnectionTest {
+
+}
--- src/main/java/com/mysite/todolist/TodolistApplication.java
+++ src/main/java/com/mysite/todolist/TodolistApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class TodolistApplication {
 
     public static void main(String[] args) {
+        System.out.println("test2");
         SpringApplication.run(TodolistApplication.class, args);
     }
-
 }
